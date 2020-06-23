0dceff4b0974e350b96a23bacda7e04fa9d8b57b
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2020-06-23 21:55:03 +0100

    Updates

---
+++ precache-manifest.06db10dfa7bed345e407c9724c492c0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f981c4c427616d3c44a25afb1ec3ac5f",
+    "url": "/react.io/index.html"
+  },
+  {
+    "revision": "c63ae172ae15c16cc0d1",
+    "url": "/react.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ad63c97ab6aa2dd82e4",
+    "url": "/react.io/static/js/2.6470b349.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react.io/static/js/2.6470b349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63ae172ae15c16cc0d1",
+    "url": "/react.io/static/js/main.95312058.chunk.js"
+  },
+  {
+    "revision": "e1fc230f023a6996cd36",
+    "url": "/react.io/static/js/runtime-main.3582067e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
